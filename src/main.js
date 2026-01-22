2485a79d1751c5b049f27aca72b67714dbbe647d
Anderson Martins <atmmoreira.rj@gmail.com>
2026-01-22 19:22:36 -0300

    feat: implement PlaceFinder class for geolocation and address handling

--- src/main.js
+++ src/main.js
@@ -1,1 +1,33 @@
 import "./style.css";
+
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector("form");
+    const locateUserBtn = document.getElementById("locate-btn");
+
+    locateUserBtn.addEventListener("click", this.locateUserHandler);
+    addressForm.addEventListener("submit", this.findAddressHandler);
+  }
+
+  locateUserHandler() {
+    if (!navigator.geolocation) {
+      alert("Geolocation is not supported by your browser.");
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (successResult) => {
+        const coordinates = {
+          lat: successResult.coords.latitude,
+          lng: successResult.coords.longitude,
+        };
+      },
+      (error) => {
+        alert("Could not locate you. Please enter an address manually!");
+      },
+    );
+  }
+
+  findAddressHandler() {}
+}
+new PlaceFinder();
